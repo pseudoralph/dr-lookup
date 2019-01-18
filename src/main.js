71702fbafb5e8f95ffa089dbf63937a8dc331f7e
Ralph Perdomo <ralph@prdmo.com>
2019-01-18 10:47:53 -0800

    save

--- src/main.js
+++ src/main.js
@@ -1,23 +1,37 @@
 import { DrSearch } from './dr-search';
 
-document.onreadystatechange = function() {
-  if (document.readyState === 'interactive') {
+// document.onreadystatechange = function() {
+//   if (document.readyState === 'interactive') {
 
-    init();
-  }
+//     // init();
 
-};
+//     let drsearch = new DrSearch();
+//     drsearch.search().then(function(results) {
+//       console.log('hi')
+//       console.log(results)
+//     })
+//   }
+
+// };
 
 
-function init() {
+
   console.log('ready!');
 
-  const drsearch = new DrSearch();
 
-  drsearch.search().then(function(response) {
-    console.log(response)
-  });
+// let xhr = new XMLHttpRequest();
+
+// xhr.addEventListener("readystatechange", function () {
+//   if (this.readyState === 4) {
+//     console.log(JSON.parse(this.responseText));
+//   }
+// });
+
+// xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=1&user_key=5f3f3999115c62d6f07974ce335107a9");
+// xhr.send();
 
 
-}
+
+
+
 
