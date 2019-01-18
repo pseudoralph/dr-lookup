bae2375d6c7eaf22bc939537a1197d297b32c4de
Ralph Perdomo <ralph@prdmo.com>
2019-01-18 10:59:03 -0800

    make successful dr search object returning a promise

--- src/dr-search.js
+++ src/dr-search.js
@@ -1,50 +1,23 @@
 import { apiKey } from './.env';
 
 class DrSearch {
-
-  search() {
+  search(location='37.773%2C-122.413%2C100', user_location='37.773%2C-122.413', q="") {
     return new Promise(function(resolve, reject) {
       const cors = 'https://cors-anywhere.herokuapp.com/';
       const request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`;
-
+      const url = `${cors}https://api.betterdoctor.com/2016-03-01/doctors?query=${q}&location=${location}&user_location=${user_location}&skip=0&limit=10&user_key=${apiKey}`;
 
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-
+        } else { reject(Error(request.statusText)); }
+      };
 
       request.open('GET', url);
-      request.setRequestHeader("Accept", "application/json");
-      request.setRequestHeader("Cache-Control", "no-cache");
-      // request.setRequestHeader("Access-Control-Allow-Origin", "http://localhost:8080/");
-
       request.send();
-      
-
     });
   }
 }
 
 export { DrSearch };
-
-
-let xhr = new XMLHttpRequest();
-
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4) {
-    console.log(JSON.parse(this.responseText));
-  }
-});
-
-xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=1&user_key=5f3f3999115c62d6f07974ce335107a9");
-xhr.send();
-
-
-
-
 
--- src/main.js
+++ src/main.js
@@ -1,37 +1,15 @@
 import { DrSearch } from './dr-search';
 
-// document.onreadystatechange = function() {
-//   if (document.readyState === 'interactive') {
+document.onreadystatechange = function() {
+  if (document.readyState === 'interactive') {
 
-//     // init();
+    console.log('readssssssy!');
+    
+    const search = new DrSearch();
+    search.search().then(function(response) {
+      console.log(JSON.parse(response));
+    });
 
-//     let drsearch = new DrSearch();
-//     drsearch.search().then(function(results) {
-//       console.log('hi')
-//       console.log(results)
-//     })
-//   }
+  }
 
-// };
-
-
-
-  console.log('ready!');
-
-
-// let xhr = new XMLHttpRequest();
-
-// xhr.addEventListener("readystatechange", function () {
-//   if (this.readyState === 4) {
-//     console.log(JSON.parse(this.responseText));
-//   }
-// });
-
-// xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=1&user_key=5f3f3999115c62d6f07974ce335107a9");
-// xhr.send();
-
-
-
-
-
-
+};
