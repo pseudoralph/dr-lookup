f0b2f38b80e84b2a22f3adbdf9b17f67a7fc4d10
Ralph Perdomo <ralph@prdmo.com>
2019-01-18 13:27:31 -0800

    add dr info card

--- src/dr-search-ui.js
+++ src/dr-search-ui.js
@@ -1,7 +1,22 @@
-function ui() {
-  console.log('ok');
+function buildDrCard(drInfo) {
+  let card = document.createElement('div');
+  card.setAttribute('class', 'card col-md-3 dr-card');
+
+  card.innerHTML = `<div class="card-body">
+  <h5 class="card-title">${drInfo.name}</h5>
+  </div>
+  <ul class="list-group list-group-flush">
+  <li class="list-group-item"><i class="fas fa-map-marked-alt"></i> ${drInfo.address}<br><span style="padding-right: 24px;"></span> ${drInfo.address2}</li>
+  <li class="list-group-item"><i class="fas fa-phone"></i> ${drInfo.phone}</li>
+  </ul>
+  <div class="card-body">
+  ${ drInfo.acceptingNewPatients ? '<i class="fas fa-user-plus"></i> Accepting patients' : '<i class="fas fa-user-slash"></i> Not accepting patients<br>'}
+  </div>`;
+
+  return card;
 }
 
+export { buildDrCard };
 
 
-export { ui };
+// <br><span style="padding-right: 24px;"></span> Portland, OR 97202
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { DrSearch } from './dr-search';
-import { ui } from './dr-search-ui';
+import { buildDrCard } from './dr-search-ui';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
@@ -13,14 +13,40 @@ document.onreadystatechange = function() {
 function search() {
   const query = document.getElementById('query').value;
   const loading = document.getElementById('loading');
+  const responseOutput = document.getElementById('response');
+  responseOutput.innerHTML = "";
   loading.classList.remove('hidden');
 
-  const search = new DrSearch();
-  search.search('45.517%2C-122.670%2C25', '45.517%2C-122.670', query).then(function(response) {
-    loading.classList.add('hidden');
+  const ailment = new DrSearch();
+  ailment.search('45.517%2C-122.670%2C25', '45.517%2C-122.670', query)
+    .then(function(response) {
+      loading.classList.add('hidden');
 
-    console.log(JSON.parse(response));
-  });
+      const results = JSON.parse(response)
+
+      // first name, last name, address, phone number, website and whether or not the doctor is accepting new patients 
+      if (results.meta.total > 0) {
+        results.data.forEach(function(dr) {
+
+          const payload = {
+            name: `${dr.profile.first_name} ${dr.profile.last_name}${ (dr.profile.title) ? `, ${dr.profile.title}`: ''}`,
+            address: `${dr.practices[0].visit_address.street}`,
+            address2: `${dr.practices[0].visit_address.city}, ${dr.practices[0].visit_address.state} ${dr.practices[0].visit_address.zip}`,
+            phone: dr.practices[0].phones[0].number,
+            acceptingNewPatients: dr.practices[0].accepts_new_patients 
+          }
+
+          responseOutput.append(buildDrCard(payload));     
+        })
+      }
+
+      console.log(JSON.parse(response));
+    })
+    .catch(function(response) {
+      loading.classList.add('hidden');
+
+      console.log(`error!\n${response}`);
+    });
 
 
 }
