e6e4a75f8b622d09ed715173050dea1346da2964
Ralph Perdomo <ralph@prdmo.com>
2019-01-21 10:54:35 -0800

    enable use of dotenv-webpack plugin

--- src/dr-search.js
+++ src/dr-search.js
@@ -1,10 +1,8 @@
-import { apiKey } from '../.env';
-
 class DrSearch {
   search(location, user_location, name, q) {
     return new Promise(function(resolve, reject) {
-      let cors = (true) ? 'https://cors-anywhere.herokuapp.com/' : '';
-      const url = `${cors}https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${q}&location=${location}&user_location=${user_location}&skip=0&limit=25&user_key=${apiKey}`;
+      const cors = (false) ? 'https://cors-anywhere.herokuapp.com/' : ''; //used to test cors proxy server
+      const url = `${cors}https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${q}&location=${location}&user_location=${user_location}&skip=0&limit=25&user_key=${process.env.apiKey}`;
       const request = new XMLHttpRequest();
 
       request.onload = function() {
@@ -13,7 +11,7 @@ class DrSearch {
         } else { reject(Error(request.statusText)); }
       };
 
-      request.open('GET', url);
+      request.open('GET', url, true);
       request.send();
     });
   }
